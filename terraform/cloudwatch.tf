7d7bc901f2ec2016a040b241f063c89010d2883a
taiki-kuraishi <kuraishi.taiki0@gmail.com>
2025-02-21 23:40:07 +0900

    Removed all existing Terraform resources

--- terraform/cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-# minecraft cloudwatch
-resource "aws_cloudwatch_log_group" "minecraft_log_group" {
-  name              = "/tsar/ecs/minecraft"
-  retention_in_days = 30
-}
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "secret_value" {
-  value     = nonsensitive(data.sops_file.secrets.data)
-  sensitive = false
-}
